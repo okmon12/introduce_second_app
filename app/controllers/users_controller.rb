25888319c8e8ba444e15031a8205a5618d8e8dad
okmon12 <143723986+okmon12@users.noreply.github.com>
2024-01-23 23:55:40 +0900

    修正１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      puts current_user.inspect
       redirect_to users_path
     else
       render :new, status: :unprocessable_entity
@@ -27,7 +26,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :nickname)
   end
   def comment_params
-    user_id = current_user.id
     params.require(:comment).permit(:birthdate, :strengths, :weaknesses, :image, :message).merge(user_id: current_user.id)
   end
 end
